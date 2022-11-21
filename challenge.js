1922e57767430c396b552d848327ee103373192e
Mara Gabriela <marazumaeta@gmail.com>
2022-11-21 23:00:18 +0100

    He creado tres archivos nuevos

---
+++ challenge.js
@@ -0,0 +1,24 @@
+/* 
+  si utilizáis algún import en vuestra solución, recordad que hay que indicarle a node 
+  que estamos utilizando módulos. Para ello, debemos incluir el fichero package.json que 
+  veis en este repositorio. En caso de que no os funcione, contactadme por discord.
+*/
+
+
+//Partiremos de un listado de alumnos inicial:
+const students = [{
+    age: 32,
+    examScores: [],
+    gender: 'male',
+    name: 'edu'
+  },
+  {
+    age: 29,
+    examScores: [],
+    gender: 'female',
+    name: 'silvia'
+  }]
+  
+  const availableMaleNames = ['pepe', 'juan', 'victor', 'Leo', 'francisco', 'carlos'];
+  const availableFemaleNames = ['cecilia', 'ana', 'luisa', 'silvia', 'isabel', 'virginia'];
+  const availableGenders = ['male', 'female'];
